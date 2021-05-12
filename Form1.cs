49972085ea69f864c3ab39c73ef61bd5ddbafb7f
curtis1366 <81329592+curtis1366@users.noreply.github.com>
2021-05-12 12:33:56 -0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -63,9 +63,7 @@ namespace Matching_Game
         /// <param name="e"></param>
         private void label_click(object sender, EventArgs e)
         {
-            // The timer is only on after two non-matching  
-            // icons have been shown to the player,  
-            // so ignore any clicks if the timer is running
+            //this will timer when user choice two non-matching terms. 
             if (timer1.Enabled == true)
                 return;
 
@@ -73,16 +71,10 @@ namespace Matching_Game
 
             if (clickedLabel != null)
             {
-                // If the clicked label is black, the player clicked 
-                // an icon that's already been revealed -- 
-                // ignore the click 
+                //A click will be ignored if it is already black. 
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                // If firstClicked is null, this is the first icon  
-                // in the pair that the player clicked, 
-                // so set firstClicked to the label that the player  
-                // clicked, change its color to black, and return
                 if (firstClicked == null)
                 {
                     firstClicked = clickedLabel;
@@ -90,19 +82,14 @@ namespace Matching_Game
                     return;
                 }
 
-                // If the player gets this far, the timer isn't 
-                // running and firstClicked isn't null, 
-                // so this must be the second icon the player clicked 
-                // Set its color to black
+                // this will deal with the second click. 
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
                 // Check to see if the player won
                 CheckForTheWinner();
 
-                // If the player clicked two matching icons, keep them  
-                // black and reset firstClicked and secondClicked  
-                // so the player can click another icon 
+               // if a macthis found. this will reset the cliked varables.  
                 if (firstClicked.Text == secondClicked.Text)
                 {
                     firstClicked = null;
@@ -111,19 +98,15 @@ namespace Matching_Game
                     return;
                 }
 
-                // If the player gets this far, the player  
-                // clicked two different icons, so start the  
-                // timer (which will wait three quarters of  
-                // a second, and then hide the icons)
+                // if the player picks two that are not correct
+                //that it will wait 3/4 of a second the clear them. 
                 timer1.Start();
             }
         }
 
         /// <summary> 
-        /// This timer is started when the player clicks  
-        /// two icons that don't match, 
-        /// so it counts three quarters of a second  
-        /// and then turns itself off and hides both icons 
+        /// if the player picks two that are not correct
+        /// that it will wait 3/4 of a second the clear them. 
         /// </summary> 
         /// <param name="sender"></param>
         /// <param name="e"></param>
